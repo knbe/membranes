e7dcaacdb8205d639b6c51cb4ebe0f4f78e57213
K A <nrv2209@protonmail.com>
2022-11-22 12:35:14 -0500

    incorporate boltz weights

--- constraints.c
+++ constraints.c
@@ -4,10 +4,13 @@ vector gs_distance_constraint (vector pos1, vector pos2,
 	double w1, double w2, double constraint)
 {
 	vector r = vector_subt(pos1, pos2);
-	vector rHat = vector_unit(r); 
-       	double rMag = vector_mag(r);
+	vector r_u = vector_unit(r); 
+       	double r_m = vector_mag(r);
 
-	vector correction = vector_mult(rHat, (rMag - constraint));
+	vector correction = vector_mult(
+			w1, 
+			vector_mult(r_u, (r_m - constraint))
+			);
 
 	return correction; 	
 }
